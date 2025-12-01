90765508d54cc08828dbe878e93dfbc5822bb151
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-12-01 15:51:21 -0300

    Initial Commit

---
+++ TermoDeBloqueio/TermoDeBloqueioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermoDeBloqueioApp.swift
+//  TermoDeBloqueio
+//
+//  Created by Lucas Dal Pra Brascher on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TermoDeBloqueioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
